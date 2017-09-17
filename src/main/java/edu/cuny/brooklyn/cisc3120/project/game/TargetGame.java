ab994d8e9b8d40bda2496bfddd3122fdc631465c
Mike <mike.grinspan@gmail.com>
2017-09-16 22:45:58 -0400

    Added command line arguments, setup maven, and reformatted code

--- src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGame.java
+++ src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGame.java
@@ -8,29 +8,36 @@ import org.slf4j.LoggerFactory;
 
 public class TargetGame {
 	private static Logger logger = LoggerFactory.getLogger(TargetGame.class);
-	
-	private final int GAME_TARGET_AREA_WIDTH = 80;
-	private final int GAME_TARGET_AREA_HEIGHT = 25;
+
+	private int GAME_TARGET_AREA_HEIGHT;
+	private int GAME_TARGET_AREA_WIDTH;
 
 	GameBoard gameBoard;      // having its own dimension: cells
 	GameDisplay gameDisplay;  // having its own dimension: cells to characters
 	private Random rng;
 	private Scanner in;
-	
+
 	public TargetGame() {
-		gameBoard = new GameBoard(GAME_TARGET_AREA_HEIGHT, GAME_TARGET_AREA_WIDTH);
+		this(25, 80);
+	}
+
+	public TargetGame(int height, int width) {
+		GAME_TARGET_AREA_HEIGHT = height;
+		GAME_TARGET_AREA_WIDTH = width;
+
+		gameBoard = new GameBoard(height, width);
 		gameDisplay = new GameDisplay(gameBoard);
 		rng = new Random();
 		in = new Scanner(System.in);
 		in.useDelimiter("(\\p{javaWhitespace}|,)+");
 	}
-	
+
 	public void play() {
 		boolean won = false;
 		setTarget();
 		gameBoard.plotBorder();
-		gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1, "Enter your target position (x, y):");
-		while(!won) {
+		gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT - 1, "Enter your target position (x, y):");
+		while (!won) {
 			gameDisplay.draw();
 
 			int xGuess = in.nextInt();
@@ -38,21 +45,21 @@ public class TargetGame {
 			logger.debug("Player guessed x = " + xGuess + ", y =" + yGuess + ".");
 			if (gameBoard.getCell(xGuess, yGuess) == 'X') {
 				gameBoard.plotBorder();
-				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1, "You won. Game over.");
+				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT - 1, "You won. Game over.");
 				won = true;
 			} else {
 				gameBoard.plotBorder();
-				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1,"Try again. Enter your target position (x, y): ");
+				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT - 1, "Try again. Enter your target position (x, y): ");
 			}
 			gameDisplay.draw();
 		}
 	}
-	
+
 	private void setTarget() {
 		int x = rng.nextInt(GAME_TARGET_AREA_WIDTH);
 		int y = rng.nextInt(GAME_TARGET_AREA_HEIGHT);
 		gameBoard.setCell(x, y, 'X');
 		logger.debug("Target: " + x + "," + y);
 	}
-	
+
 }
--- src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGameLauncher.java
+++ src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGameLauncher.java
@@ -1,11 +1,35 @@
 package edu.cuny.brooklyn.cisc3120.project.game;
 
+import org.apache.commons.cli.*;
 
-public class TargetGameLauncher 
-{
-    public static void main( String[] args )
-    {
-        TargetGame game = new TargetGame();
-        game.play();
-    }
+public class TargetGameLauncher {
+	public static void main(String[] args) {
+		TargetGame game;
+
+		CommandLineParser parser = new DefaultParser();
+		Options options = new Options();
+
+		try {
+			int height = 25;
+			int width = 80;
+
+			options.addOption("h", "window-height", true, "Used to set the height of the game's board.");
+			options.addOption("w", "window-width", true, "Used to set the width of the game's board.");
+
+			CommandLine parsed = parser.parse(options, args);
+
+			if (parsed.hasOption("h") && parsed.getOptionValue("h").matches("^\\d+$")) {
+				height = Integer.parseInt(parsed.getOptionValue("h"));
+			}
+			if (parsed.hasOption("w") && parsed.getOptionValue("w").matches("^\\d+$")) {
+				width = Integer.parseInt(parsed.getOptionValue("w"));
+			}
+
+			game = new TargetGame(height, width);
+		} catch (ParseException exception) {
+			game = new TargetGame();
+		}
+
+		game.play();
+	}
 }
