699418574b860d40007710c86c9e9a47e510e974
Michael Grinspan <mike.grinspan@gmail.com>
2017-09-21 15:54:08 -0400

    Minor cleanup

--- src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGameLauncher.java
+++ src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGameLauncher.java
@@ -4,18 +4,16 @@ import org.apache.commons.cli.*;
 
 public class TargetGameLauncher {
 	public static void main(String[] args) {
-		TargetGame game;
-
 		CommandLineParser parser = new DefaultParser();
 		Options options = new Options();
 
+		int height = 25;
+		int width = 80;
+
+		options.addOption("h", "window-height", true, "Used to set the height of the game's board.");
+		options.addOption("w", "window-width", true, "Used to set the width of the game's board.");
+
 		try {
-			int height = 25;
-			int width = 80;
-
-			options.addOption("h", "window-height", true, "Used to set the height of the game's board.");
-			options.addOption("w", "window-width", true, "Used to set the width of the game's board.");
-
 			CommandLine parsed = parser.parse(options, args);
 
 			if (parsed.hasOption("h") && parsed.getOptionValue("h").matches("^\\d+$")) {
@@ -24,11 +22,11 @@ public class TargetGameLauncher {
 			if (parsed.hasOption("w") && parsed.getOptionValue("w").matches("^\\d+$")) {
 				width = Integer.parseInt(parsed.getOptionValue("w"));
 			}
-
-			game = new TargetGame(height, width);
 		} catch (ParseException exception) {
-			game = new TargetGame();
+			// do nothing on parse error
 		}
+
+		TargetGame game = new TargetGame(height, width);
 
 		game.play();
 	}
